0dcc274e5974bd557546bfeaeea142cc1d424dbf
masterBaster <bartek.baster@gmail.com>
2018-10-24 21:52:25 +0200

    working with showExtended function added optional params

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/User';
 
 @Component({
   selector: 'app-users',
@@ -7,10 +8,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
+  title = 'templete sytax';
+  users: User[];
+  showExtended: boolean = true;
+  loaded: boolean = true;
+
   constructor() { }
 
   ngOnInit() {
-    console.log('ngOnInit...')
+    this.users = [
+      {
+        firstName: 'John',
+        lastName: 'Adams',
+        age: 30,
+        address: {
+          street: '50 Main st.',
+          city: 'Boston',
+          state: 'MA'
+        }
+      },
+      {
+        firstName: 'Paul',
+        lastName: 'Mark',
+        age: 31,
+        address: {
+          street: '356 Main st.',
+          city: 'Boston',
+          state: 'MA'
+        }
+      },
+      {
+        firstName: 'Mick',
+        lastName: 'Kovnacki',
+        age: 34,
+        address: {
+          street: '10 Main st.',
+          city: 'Boston',
+          state: 'MA'
+        }
+      },
+    ];
+    this.loaded = true;
+
+    this.addUser({
+      firstName: 'David',
+      lastName: 'Malkowski'
+      })
   }
 
+  addUser(user: User) {
+    this.users.push(user);
+  }
 }
